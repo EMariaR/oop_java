425331ff4aea698d51f7ce8c323a47e0f51aecf3
eMariaR <elo-maria.roots@khk.ee>
2018-11-15 11:02:16 +0200

    ringtüüpi objekti kirjelduse loomine ja rakendatud konkreetse ringtüüpi objekti loomiseks

---
+++ pohimoisted/src/ring.java
@@ -0,0 +1,10 @@
+public class Ring {
+    public Double raadius;
+    public Double x;
+    public Double y;
+
+    public void pindala() {
+        Double s = 3.14 * raadius * raadius;
+        System.out.println(s);
+    }
+}
