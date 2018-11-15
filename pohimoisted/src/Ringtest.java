d668092708093f15d30f09785df9e402887ada37
eMariaR <elo-maria.roots@khk.ee>
2018-11-15 11:10:43 +0200

    ringtüüpi objekti kirjeldus täiendatud konstruktoiriga, mis võimaldab luua ringi raadiusega 0

--- pohimoisted/src/Ringtest.java
+++ pohimoisted/src/Ringtest.java
@@ -1,7 +1,7 @@
 public class Ringtest {
     public static void  main(String[] args) {
         Ring minuRing = new Ring();
-        minuRing.raadius = 2.7;
+     //   minuRing.raadius = 2.7;
         minuRing.pindala();
     }
 }
