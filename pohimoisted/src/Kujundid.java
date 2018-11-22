d2d8547de9c17081f2f8143bfebb41c49a50bdf3
eMariaR <elo-maria.roots@khk.ee>
2018-11-22 09:21:59 +0200

    loodud pärilus-kujundite kirjeldus

---
+++ pohimoisted/src/Kujundid.java
@@ -0,0 +1,11 @@
+public class Kujundid {
+    public static void main(String[] args) {
+        Kujund ring = new Kujund("Ring");
+        Kujund ruut =new Kujund("Ruut");
+        Kujund kolmnurk =new Kujund("Kolmnurk");
+
+        ring.väljastaKirjeldus();
+        ruut.väljastaKirjeldus();
+        kolmnurk.väljastaKirjeldus();
+    }
+}
