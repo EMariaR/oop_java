a95ed32572102f2921076a08aece87f84d1836c2
eMariaR <elo-maria.roots@khk.ee>
2018-11-28 10:03:40 +0200

    lagi

---
+++ pohimoisted/src/Lagi.java
@@ -0,0 +1,18 @@
+public class Lagi {
+    private Integer korgus;
+    private String varv;
+
+    public Lagi(Double korgus, String varv) {
+        this.korgus = korgus;
+        this.varv = varv;
+    }
+
+    public Integer getKorgus() {
+        return korgus;
+    }
+
+    public String getVarv() {
+        return varv;
+    }
+
+}
