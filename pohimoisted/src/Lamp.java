4ea689036daf8955043877a76c559a5e45d7d770
eMariaR <elo-maria.roots@khk.ee>
2018-11-28 10:03:25 +0200

    lamp

---
+++ pohimoisted/src/Lamp.java
@@ -0,0 +1,25 @@
+public class Lamp {
+    private String nimetus;
+    private Boolean patarei;
+    private Boolean sisseLulitatud;
+
+    public Lamp(String nimetus, Boolean patarei) {
+        this.nimetus = nimetus;
+        this.patarei = patarei;
+    }
+
+    public void lulita() {
+        if(sisseLulitatud == false) {
+            sisseLulitatud = true;
+        }
+        System.out.println("Lamp on sisse lulitatud");
+    }
+        else if
+                sisseLulitatud == false) {
+
+     }
+
+
+}
+
+
