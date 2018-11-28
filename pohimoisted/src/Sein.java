6af6fa1de7d2b37fb08619fac0833e43bf4bb51c
eMariaR <elo-maria.roots@khk.ee>
2018-11-28 09:39:33 +0200

    Sein

---
+++ pohimoisted/src/Sein.java
@@ -0,0 +1,11 @@
+public class Sein {
+    private String suund;
+
+    public Sein(String suund) {
+        this.suund = suund;
+    }
+
+    public String getSuund() {
+        return suund;
+    }
+}
